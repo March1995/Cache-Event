8fbccf9717c0fdccc9b55ec2194f09efd62a01d5
wangyingbo <1501987783@qq.com>
2023-04-28 09:54:49 +0800

    设置执行顺序

--- cache-event-core/src/main/java/com/umax/cache/event/core/registry/WebCacheEventRegistry.java
+++ cache-event-core/src/main/java/com/umax/cache/event/core/registry/WebCacheEventRegistry.java
@@ -1,11 +1,13 @@
 package com.umax.cache.event.core.registry;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 
 /**
  * @author wangyingbo
  * @date 2023-01-30 17:13
  **/
+@Order(Integer.MIN_VALUE)
 public class WebCacheEventRegistry extends EventRegistry implements CommandLineRunner {
 
     @Override
